71c6482f27d35755f4e348a4ac3c1ba48ef7d4e6
Nnamdi Kalu <nnamdi.kalu@gmail.com>
2016-12-23 17:48:00 -0500

    simple error handling

--- TipCalc.py
+++ TipCalc.py
@@ -1,5 +1,22 @@
-Meal = float(input('Price of Meal:'))
-TipPercent = float(input('Tip Percentage:'))
+def mealPrice():
+    try:
+        meal = float(input('Price of Meal:'))
+        return meal
+    except NameError:
+        print('Please use integers')
+        mealPrice()
+
+def tipPrice():
+    try:
+        tipPercent = float(input('Tip Percentage:'))
+        return tipPercent
+    except NameError:
+        print('Please use integers')
+        tipPrice()
+
+Meal = mealPrice()
+TipPercent = tipPrice()
+
 Tip = Meal * (TipPercent/100)
 Total = Meal + Tip
 print('Tip= ' + str(round(Tip,2)))
